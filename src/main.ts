719777657710d3ad907c216b906c3250ea046fd6
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2020-04-17 22:14:09 +0300

    feat(main.ts) add validation pipe and configure port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './modules/app.module';
+import { ConfigService } from './modules/config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const PORT = app.get(ConfigService).get('PORT', '3000');
+  await app.listen(PORT);
 }
 bootstrap();
