dba6234c813266f23264dd6233d2b43ac405483c
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2020-04-18 11:57:32 +0300

    feat(modules:owner:schemas) create owner schema

---
+++ src/modules/owner/schemas/owner.schema.ts
@@ -0,0 +1,20 @@
+import * as mongoose from 'mongoose';
+
+export const OwnerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    purchaseDate: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
