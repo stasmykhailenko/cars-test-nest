97afb820ff788053cf047712e661900abbdf1fe3
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2020-04-23 23:46:59 +0300

    feat(main.ts) set default timezone

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
+import * as moment from 'moment-timezone';
+
 import { AppModule } from './modules/app.module';
 import { ConfigService } from './modules/config/config.service';
 
@@ -13,6 +15,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  moment.tz.setDefault('UTC');
 
   const PORT = app.get(ConfigService).get('PORT', '3000');
   await app.listen(PORT);
