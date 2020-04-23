1bbf51d0a09b2c8cd3e33d9ec1a1ce8ab763ae46
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2020-04-23 23:50:47 +0300

    feat(modules:owner) add functionality to delete owners

--- src/modules/owner/owner.service.ts
+++ src/modules/owner/owner.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
+import * as moment from 'moment';
+
 import { Model } from 'mongoose';
+
+import { CarService } from '../car/car.service';
 
 import { OwnerDoc } from './models/owner.model';
 
@@ -10,6 +14,21 @@ export class OwnerService {
   constructor(
     @InjectModel('Owner')
     private readonly ownerModel: Model<OwnerDoc>,
+    private readonly carService: CarService,
   ) {
+  }
+
+  async removeOwners() {
+    const searchDate = moment().subtract(18, 'months').toDate();
+
+    const owners = await this.ownerModel.find({
+      purchaseDate: {
+        $lt: searchDate,
+      },
+    });
+
+    const ids = owners.map(owner => owner._id);
+
+    return this.carService.deleteOwners(ids);
   }
 }
