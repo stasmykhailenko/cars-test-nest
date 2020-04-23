a3567bfce0bce58cfddb2a06f5fd84ed3b7dbf26
Stas Mykhailenko <stas.mykhailenko@gmail.com>
2020-04-23 23:51:41 +0300

    feat(modules:core) create task service

--- src/modules/core/core.module.ts
+++ src/modules/core/core.module.ts
@@ -1,10 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
 
 import { SharedModule } from '../shared/shared.module';
 import { ConfigModule } from '../config/config.module';
 
 import { MongooseConfigService } from './mongoose/mongoose-config.service';
+import { TasksService } from './services/tasks.service';
 
 @Module({
   imports: [
@@ -14,6 +16,13 @@ import { MongooseConfigService } from './mongoose/mongoose-config.service';
       imports: [ConfigModule],
       useClass: MongooseConfigService,
     }),
+    ScheduleModule.forRoot(),
+  ],
+  providers: [
+    TasksService,
+  ],
+  exports: [
+    TasksService,
   ],
 })
 export class CoreModule implements NestModule {
